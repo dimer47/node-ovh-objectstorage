3c578cfc045ef33d42f801267ad57a28e403c2df
Guillaume GABOREL <guillaume.gaborel@s3pweb.com>
2018-09-17 17:18:47 +0200

    feat(container): get container metadata test

--- test/container.test.js
+++ test/container.test.js
@@ -5,9 +5,18 @@ import test from 'ava/index'
 const config = require('config')
 const OVHStorage = require('../lib/OVHObjectStorage')
 
+test('Get container metadata', async t => {
+  let storage = new OVHStorage(config.storageConfig)
+  await storage.connection()
+
+  let metadata = await storage.container().info('s3pweb-images')
+
+  t.truthy(metadata, 'Metadata should exist')
+  t.truthy(metadata['x-container-object-count'], 'Object count key should exist')
+})
+
 test('List container files', async t => {
   let storage = new OVHStorage(config.storageConfig)
-
   await storage.connection()
 
   let infos = await storage.container().list('s3pweb-images')
