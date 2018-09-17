793c85384dab41c40f502884c023c1eae91e642f
Guillaume GABOREL <guillaume.gaborel@s3pweb.com>
2018-09-17 16:04:04 +0200

    feat(container): get files list from container

--- test/authent.test.js
+++ test/authent.test.js
@@ -5,12 +5,12 @@ import test from 'ava/index'
 const config = require('config')
 const OVHStorage = require('../lib/OVHObjectStorage')
 
-test('Login to OVH - Object Storagz', async t => {
+test('Login to OVH', async t => {
   let storage = new OVHStorage(config.storageConfig)
 
   let values = await storage.connection()
 
-  t.truthy(values, 'NOPE')
-  t.truthy(values.token, 'NOPE token')
-  t.truthy(values.endpoint, 'NOPE endpoint')
+  t.truthy(values, 'Values should exist')
+  t.deepEqual(Object.keys(values.token), ['issued_at', 'expires', 'id', 'tenant', 'audit_ids'], 'Token should contains all the keys')
+  t.deepEqual(Object.keys(values.endpoint), ['adminURL', 'region', 'id', 'internalURL', 'publicURL'], 'Endpoint should contains all the keys')
 })
