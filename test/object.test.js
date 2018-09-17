4ccd9026161d01fc173e7ffb658fbbb7c87014f4
Guillaume GABOREL <guillaume.gaborel@s3pweb.com>
2018-09-17 17:11:15 +0200

    feat(object): get file content

--- test/object.test.js
+++ test/object.test.js
@@ -5,7 +5,7 @@ import test from 'ava/index'
 const config = require('config')
 const OVHStorage = require('../lib/OVHObjectStorage')
 
-test('File metadata - Object Storage', async t => {
+test('Get file metadata', async t => {
   let storage = new OVHStorage(config.storageConfig)
   await storage.connection()
 
@@ -14,4 +14,13 @@ test('File metadata - Object Storage', async t => {
   t.truthy(objectInfo, 'Metadatas should exist')
   t.deepEqual(objectInfo['content-length'], ['68899'], 'Content length should be 68899')
   t.deepEqual(objectInfo['content-type'], ['image/png'], 'Content length should be image/png')
+})
+
+test('Get file content', async t => {
+  let storage = new OVHStorage(config.storageConfig)
+  await storage.connection()
+
+  let objectInfo = await storage.object().get('s3pweb-images', 'Advanced_use_case_1.png', 'image.png')
+
+  t.truthy(objectInfo, 'Metadatas should exist')
 })
